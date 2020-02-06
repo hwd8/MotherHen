23f245846251add00f47de57d528d8a227e61d54
AlisonT <alison.tong@mail.utoronto.ca>
2020-02-06 03:05:54 -0500

    Alert and close tab that matches forbidden list

--- background.js
+++ background.js
@@ -1,9 +1,19 @@
-chrome.runtime.onInstalled.addListener(function() {
-    console.log("I am installed");
-    chrome.storage.sync.set({"color": '#3aa757'}, function() {
-        console.log("STORED");
+const forbidden = ["facebook", "youtube", "discord", "bilibili", "twitter", "tumblr"];
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        tabs.forEach(t => {
+            forbiddenTab = forbidden.filter(f => t.url.includes(f))[0];
+            if(forbiddenTab) {
+                alert(forbiddenTab.toUpperCase() + " IS CANCELLED!");
+                chrome.tabs.remove(t.id, function() {
+                    if(chrome.runtime.lastError) {
+                    }
+                });
+            }
+        });
     });
-    chrome.storage.sync.get(["color"], function(value) {
-        console.log(value);
-    })
 });
